e0af1f7743295b8c9047b3a6e99516ee3db78994
Rajiv Roy <rajivpune18@gmail.com>
2026-01-14 21:54:58 +0530

    Comment out useEffect example in useeffect_timing.js
    Commented out the useEffect example for clarity.

--- useeffect_timing.js
+++ useeffect_timing.js
@@ -11,3 +11,13 @@ function App() {
     </button>
   );
 }
+
+/*
+Effect: 0
+runs only once due to Empty dependency array [] → effect runs only once
+to make it log on every change - 
+React.useEffect(() => {
+  console.log("Effect:", count);
+}, [count]);
+
+*/
